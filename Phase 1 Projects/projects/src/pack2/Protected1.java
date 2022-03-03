4ce8a844646b8adf7e0ca2085cfede0ccbf15b04
Deepsaa <deepsa.sahoo90@gmail.com>
2022-03-03 15:50:10 +0530

    phase 1 projects added

---
+++ Phase 1 Projects/projects/src/pack2/Protected1.java
@@ -0,0 +1,10 @@
+package pack2;
+import pack1.*;
+public class Protected1 extends Protected {
+
+	public static void main(String[] args) {
+		Protected1 obj = new Protected1 ();
+obj.display();
+	}
+
+}
